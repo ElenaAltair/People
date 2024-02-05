c9bfd63e88fef545d87c353813829ef52c799db8
Elena Petrova <eop79@mail.ru>
2024-02-05 09:10:58 +0300

    changed main.kt

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,16 @@
 //В переменной likes хранится число лайков
-var likes = 51
+var likes = 1211
 var lastSymbol = likes.toString().last()
 
 //В зависимости от того, что хранится в likes, приложение выводит вариант в консоль
 fun main() {
-    if (lastSymbol == '1') {
+    var twoLastSymbol = """0""";
+    if (likes.toString().length > 1) {
+        twoLastSymbol = likes.toString().substring(likes.toString().length - 2, likes.toString().length)
+    }
+    if (twoLastSymbol == "11") {
+        println("Понравилось $likes людям!")
+    } else if (lastSymbol == '1') {
         println("Понравилось $likes человеку!")
     } else {
         println("Понравилось $likes людям!")
